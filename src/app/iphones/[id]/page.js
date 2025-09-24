0812aeceaa007802e5b7ab8451c6550be356b76a
Thúlio D'avilla <thulioalmeida57@gmail.com>
2025-09-24 19:29:17 -0300

    ajuste

--- src/app/iphones/[id]/page.js
+++ src/app/iphones/[id]/page.js
@@ -3,20 +3,18 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/image"; // Mantido para logos e imagens internas
 import styles from "./details.module.css";
 import globalStoreStyles from "../../page.module.css";
 import Footer from "../../../components/Footer/Footer";
 import Loader from "../../../components/Loader/Loader";
-import { useToast } from "../../../components/Toast/ToastContext";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
-export default function iPhoneDetailsPage({ params }) {
+export default function IPhoneDetailsPage({ params }) {
   const router = useRouter();
   const { id } = params;
-  const { showToast } = useToast();
 
   const [iphone, setIphone] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -54,18 +52,31 @@ export default function iPhoneDetailsPage({ params }) {
     fetchIphoneDetails();
   }, [id]);
 
-  const handleBuyNow = () => {
-    const whatsappNumber = "5511999999999";
-    const message = `Olá! Tenho interesse no iPhone ${iphone.nome} - ${
-      iphone.armazenamento_gb
-    }GB. Preço: R$ ${parseFloat(iphone.preco_promocional || iphone.preco_tabela)
-      .toFixed(2)
-      .replace(".", ",")}. Poderíamos conversar sobre a compra?`;
-    const whatsappLink = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(
-      message
-    )}`;
+  const handleGoToCheckout = () => {
+    if (iphone) {
+      router.push(`/checkout?iphoneId=${iphone.id}`);
+    } else {
+      alert("Não foi possível iniciar a compra: iPhone não carregado.");
+    }
+  };
 
-    window.open(whatsappLink, "_blank");
+  // 🔄 Função melhorada para não exibir campos vazios
+  const formatArrayData = (data) => {
+    if (!data) return null;
+
+    if (Array.isArray(data)) {
+      return data.length > 0 ? data.join(", ") : null;
+    }
+
+    if (typeof data === "object") {
+      return Object.keys(data).length > 0 ? JSON.stringify(data) : null;
+    }
+
+    if (typeof data === "string") {
+      return data.trim() !== "" ? data : null;
+    }
+
+    return data;
   };
 
   if (loading) {
@@ -74,18 +85,15 @@ export default function iPhoneDetailsPage({ params }) {
         <header className={globalStoreStyles.header}>
           <Link href="/" className={globalStoreStyles.logoLink}>
             <Image
-              src="/sua-logo-da-loja.png"
+              src="/iphone-logo.png"
               alt="iPhones Pro Store Logo"
+              width={120}
+              height={50}
               className={globalStoreStyles.logoImage}
-              width={200}
-              height={60}
             />
           </Link>
           <nav className={globalStoreStyles.mainNav}>
-            <Link
-              href="/dashboard"
-              className={globalStoreStyles.dashboardButton}
-            >
+            <Link href="/dashboard" className={globalStoreStyles.navLink}>
               Dashboard
             </Link>
           </nav>
@@ -94,7 +102,6 @@ export default function iPhoneDetailsPage({ params }) {
           <h2 className={globalStoreStyles.pageTitle}>Detalhes do iPhone</h2>
           <Loader />
         </div>
-        <Footer />
       </div>
     );
   }
@@ -105,18 +112,15 @@ export default function iPhoneDetailsPage({ params }) {
         <header className={globalStoreStyles.header}>
           <Link href="/" className={globalStoreStyles.logoLink}>
             <Image
-              src="/sua-logo-da-loja.png"
+              src="/iphone-logo.png"
               alt="iPhones Pro Store Logo"
+              width={120}
+              height={50}
               className={globalStoreStyles.logoImage}
-              width={200}
-              height={60}
             />
           </Link>
           <nav className={globalStoreStyles.mainNav}>
-            <Link
-              href="/dashboard"
-              className={globalStoreStyles.dashboardButton}
-            >
+            <Link href="/dashboard" className={globalStoreStyles.navLink}>
               Dashboard
             </Link>
           </nav>
@@ -130,7 +134,6 @@ export default function iPhoneDetailsPage({ params }) {
             Voltar para a <Link href="/">loja</Link>.
           </p>
         </div>
-        <Footer />
       </div>
     );
   }
@@ -141,18 +144,15 @@ export default function iPhoneDetailsPage({ params }) {
         <header className={globalStoreStyles.header}>
           <Link href="/" className={globalStoreStyles.logoLink}>
             <Image
-              src="/sua-logo-da-loja.png"
+              src="/iphone-logo.png"
               alt="iPhones Pro Store Logo"
+              width={120}
+              height={50}
               className={globalStoreStyles.logoImage}
-              width={200}
-              height={60}
             />
           </Link>
           <nav className={globalStoreStyles.mainNav}>
-            <Link
-              href="/dashboard"
-              className={globalStoreStyles.dashboardButton}
-            >
+            <Link href="/dashboard" className={globalStoreStyles.navLink}>
               Dashboard
             </Link>
           </nav>
@@ -166,7 +166,6 @@ export default function iPhoneDetailsPage({ params }) {
             <Link href="/">loja</Link>.
           </p>
         </div>
-        <Footer />
       </div>
     );
   }
@@ -176,15 +175,15 @@ export default function iPhoneDetailsPage({ params }) {
       <header className={globalStoreStyles.header}>
         <Link href="/" className={globalStoreStyles.logoLink}>
           <Image
-            src="/sua-logo-da-loja.png"
+            src="/iphone-logo.png"
             alt="iPhones Pro Store Logo"
+            width={120}
+            height={50}
             className={globalStoreStyles.logoImage}
-            width={200}
-            height={60}
           />
         </Link>
         <nav className={globalStoreStyles.mainNav}>
-          <Link href="/dashboard" className={globalStoreStyles.dashboardButton}>
+          <Link href="/dashboard" className={globalStoreStyles.navLink}>
             Dashboard
           </Link>
         </nav>
@@ -195,31 +194,36 @@ export default function iPhoneDetailsPage({ params }) {
 
         <div className={styles.detailsContainer}>
           <div className={styles.imageGallery}>
-            <Image
+            {/* 🔄 Troquei para <img /> para imagens externas */}
+            <img
               src={
                 mainImage ||
                 "https://placehold.co/500x500/e0e0e0/333333?text=iPhone"
               }
               alt={iphone.nome}
-              className={styles.mainImage}
               width={500}
               height={500}
+              className={styles.mainImage}
             />
+
             <div className={styles.thumbnailGallery}>
-              {iphone.imagens_urls?.map((url, index) => (
-                <Image
-                  key={index}
-                  src={url}
-                  alt={`${iphone.nome} - ${index + 1}`}
-                  className={`${styles.thumbnail} ${
-                    url === mainImage ? styles.activeThumbnail : ""
-                  }`}
-                  onClick={() => setMainImage(url)}
-                  width={80}
-                  height={80}
-                />
-              ))}
+              {iphone.imagens_urls &&
+                iphone.imagens_urls.length > 0 &&
+                iphone.imagens_urls.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`${iphone.nome} - ${index + 1}`}
+                    width={100}
+                    height={100}
+                    className={`${styles.thumbnail} ${
+                      url === mainImage ? styles.activeThumbnail : ""
+                    }`}
+                    onClick={() => setMainImage(url)}
+                  />
+                ))}
             </div>
+
             {iphone.video_url && (
               <div className={styles.videoContainer}>
                 <h3 className={styles.sectionHeading}>Vídeo do Produto</h3>
@@ -242,6 +246,7 @@ export default function iPhoneDetailsPage({ params }) {
             <p className={styles.modelInfo}>
               {iphone.modelo} - {iphone.armazenamento_gb}GB
             </p>
+
             {iphone.preco_promocional ? (
               <>
                 <p className={styles.originalPrice}>
@@ -261,46 +266,64 @@ export default function iPhoneDetailsPage({ params }) {
                 {parseFloat(iphone.preco_tabela).toFixed(2).replace(".", ",")}
               </p>
             )}
+
             {iphone.opcoes_parcelamento && (
               <p className={styles.installmentOptions}>
-                {iphone.opcoes_parcelamento}
+                {JSON.stringify(iphone.opcoes_parcelamento)}
               </p>
             )}
 
-            <button onClick={handleBuyNow} className={styles.addToCartButton}>
-              Comprar Agora!
+            <button
+              onClick={handleGoToCheckout}
+              className={styles.addToCartButton}
+            >
+              Finalizar Compra
             </button>
 
             <div className={styles.trustBadges}>
               <h3 className={styles.badgesTitle}>Compra 100% Segura</h3>
-              <div className={styles.badgeImages}>
+              <div className={styles.badgesGrid}>
                 <Image
-                  src="/images/logo-mercadopago.png"
-                  alt="Mercado Pago"
-                  className={styles.badgeImage}
-                  width={60}
-                  height={30}
-                />
-                <Image
-                  src="/images/logo-visa.png"
-                  alt="Visa"
-                  className={styles.badgeImage}
-                  width={60}
-                  height={30}
-                />
-                <Image
-                  src="/images/logo-mastercard.png"
-                  alt="Mastercard"
-                  className={styles.badgeImage}
-                  width={60}
-                  height={30}
-                />
-                <Image
-                  src="/images/logo-ssl.png"
+                  src="/certificado-ssl.png"
                   alt="Certificado SSL"
+                  width={80}
+                  height={50}
                   className={styles.badgeImage}
-                  width={60}
-                  height={30}
+                />
+                <Image
+                  src="/compra-segura.jpg"
+                  alt="Site Seguro"
+                  width={80}
+                  height={50}
+                  className={styles.badgeImage}
+                />
+                <Image
+                  src="/logo-pagseguro.png"
+                  alt="PagSeguro"
+                  width={80}
+                  height={50}
+                  className={styles.badgeImage}
+                />
+                <Image
+                  src="/mercado-pago-seguro.png"
+                  alt="Mercado Pago"
+                  width={80}
+                  height={50}
+                  className={styles.badgeImage}
+                />
+                <Image
+                  src="/logo-visa.png"
+                  alt="Visa"
+                  width={80}
+                  height={50}
+                  className={styles.badgeImage}
+                />
+                <Image
+                  src="/logo-mastercard.png"
+                  alt="Mastercard"
+                  width={80}
+                  height={50}
+                  className={styles.badgeImage}
                 />
               </div>
             </div>
@@ -315,85 +338,85 @@ export default function iPhoneDetailsPage({ params }) {
             <div className={styles.specsSection}>
               <h3 className={styles.sectionHeading}>Especificações Técnicas</h3>
               <ul>
-                {iphone.tamanho_tela_polegadas && (
+                {formatArrayData(iphone.tamanho_tela_polegadas) && (
                   <li>
                     <span className={styles.negrito}>Tela:</span>{" "}
                     {iphone.tamanho_tela_polegadas} polegadas
                   </li>
                 )}
-                {iphone.processador_chip && (
+                {formatArrayData(iphone.processador_chip) && (
                   <li>
                     <span className={styles.negrito}>Chip:</span>{" "}
                     {iphone.processador_chip}
                   </li>
                 )}
-                {iphone.capacidade_bateria && (
+                {formatArrayData(iphone.capacidade_bateria) && (
                   <li>
                     <span className={styles.negrito}>Bateria:</span>{" "}
                     {iphone.capacidade_bateria}
                   </li>
                 )}
-                {iphone.tipo_conexao && (
+                {formatArrayData(iphone.tipo_conexao) && (
                   <li>
                     <span className={styles.negrito}>Conectividade:</span>{" "}
                     {formatArrayData(iphone.tipo_conexao)}
                   </li>
                 )}
-                {iphone.tipo_conector && (
+                {formatArrayData(iphone.tipo_conector) && (
                   <li>
                     <span className={styles.negrito}>Conector:</span>{" "}
                     {iphone.tipo_conector}
                   </li>
                 )}
-                {iphone.recursos_camera && (
+                {formatArrayData(iphone.recursos_camera) && (
                   <li>
                     <span className={styles.negrito}>Câmera:</span>{" "}
                     {formatArrayData(iphone.recursos_camera)}
                   </li>
                 )}
-                {iphone.resistencia_agua_poeira && (
+                {formatArrayData(iphone.resistencia_agua_poeira) && (
                   <li>
                     <span className={styles.negrito}>Resistência:</span>{" "}
                     {iphone.resistencia_agua_poeira}
                   </li>
                 )}
-                {iphone.sistema_operacional && (
+                {formatArrayData(iphone.sistema_operacional) && (
                   <li>
                     <span className={styles.negrito}>Sistema Operacional:</span>{" "}
                     {iphone.sistema_operacional}
                   </li>
                 )}
-                {iphone.biometria && (
+                {formatArrayData(iphone.biometria) && (
                   <li>
                     <span className={styles.negrito}>Biometria:</span>{" "}
                     {formatArrayData(iphone.biometria)}
                   </li>
                 )}
-                {iphone.dimensoes_axlxc && (
+                {formatArrayData(iphone.dimensoes_axlxc) && (
                   <li>
                     <span className={styles.negrito}>Dimensões (AxLxP):</span>{" "}
                     {iphone.dimensoes_axlxc} cm
                   </li>
                 )}
-                {iphone.peso_g && (
+                {formatArrayData(iphone.peso_g) && (
                   <li>
                     <span className={styles.negrito}>Peso:</span>{" "}
                     {iphone.peso_g}g
                   </li>
                 )}
-                {iphone.garantia_meses && (
+                {formatArrayData(iphone.garantia_meses) && (
                   <li>
                     <span className={styles.negrito}>Garantia:</span>{" "}
                     {iphone.garantia_meses} meses
                   </li>
                 )}
-                {iphone.condicao_aparelho && (
+                {formatArrayData(iphone.condicao_aparelho) && (
                   <li>
                     <span className={styles.negrito}>Condição:</span>{" "}
                     {iphone.condicao_aparelho}
                   </li>
                 )}
-                {iphone.cores_disponiveis && (
+                {formatArrayData(iphone.cores_disponiveis) && (
                   <li>
                     {" "}
                     <span className={styles.negrito}>Cores:</span>{" "}
@@ -405,7 +428,6 @@ export default function iPhoneDetailsPage({ params }) {
           </div>
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
